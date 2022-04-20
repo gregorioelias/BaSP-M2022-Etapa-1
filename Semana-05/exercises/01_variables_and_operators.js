132b23d3ab91c879b34256f1bc8a558cdc2bd7d9
Gregorio Elias <gregorio.elias24@gmail.com>
2022-04-19 23:42:03 -0300

    BaSP-2022: exercise 01-variables-and-operators done

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,20 @@
+//Excercise A
+
+var numOne = 5;
+var numTwo = 8;
+var sum = numOne + numTwo;
+console.log('ex_01_variables_and_operators_A:', sum);
+
+//Exercise B
+
+var textOne = 'Hello'
+var textTwo = 'Brother'
+var concat = textOne + ' ' + textTwo;
+console.log('ex_01_variables_and_operators_B:', concat); 
+
+//Exercise C
+
+var stringOne = 'Pedro Daniel';
+var stringTwo = 'Ramirez';
+var sumLenght = stringOne.length + stringTwo.length;
+console.log ('ex_01_variables_and_operators_C:', sumLenght);
